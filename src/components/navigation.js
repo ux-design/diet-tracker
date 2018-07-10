be38cce421a9cdd627ab7a0931a0c05982d469af
ux-design <info@design-ux.co.uk>
2018-07-10 08:33:52 +0200

    added nav animation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,21 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Logo from './logo'
-import {apiServer} from '../config'
+import {apiServer, navigationTitle} from '../config'
 
-const Navigation = ({fire, route}) => {
-  return (
-    <div 
-      className="header2 flex flex-row flex-center"
-      style={{ backgroundImage: `url(${apiServer}/assets/app/nav-bg)` }}>
-      <div className="header2__logo flex flex-center">
-        <Logo fire={fire}/> 
+class Navigation extends Component {
+  constructor() {
+    super()
+    this.state = {
+      titleClasses: 'animation__bounce'
+    }
+  }
+  _animateTitle() {
+    this.setState({
+      titleClasses: 'animation__bounce'
+    })
+    setTimeout(() => {
+      this.setState({
+        titleClasses: ''
+      })
+    },1000)
+  }
+  componentWillReceiveProps() {
+    this._animateTitle()
+  }
+  render() {
+    const {fire, route} = this.props
+    return (
+      <div 
+        className="header2 flex flex-row flex-center"
+        style={{ backgroundImage: `url(${apiServer}/assets/app/nav-bg)` }}>
+        <div className="header2__logo flex flex-center">
+          <Logo fire={fire}/> 
+        </div>
+        <h1 className={`header2__title ${this.state.titleClasses}`}>{navigationTitle[route]}</h1>
+        <div className="header2__menu flex flex-center">
+          <p>M</p>
+        </div>
       </div>
-      <h1 className="header2__title">{route}</h1>
-      <div className="header2__menu flex flex-center">
-        <p>M</p>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Navigation
--- src/config/index.js
+++ src/config/index.js
@@ -2,3 +2,8 @@ const ip = window.location.hostname
 const protocol = window.location.protocol
 export const apiServer = `${protocol}//${ip}:4000`
 export const delay = 0
+export const navigationTitle = {
+  '/dashboard': 'Dashboard',
+  '/login': 'Login',
+  '/browser-food': 'Food browser' 
+}
